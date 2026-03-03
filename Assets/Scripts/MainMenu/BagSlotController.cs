b0e9861d9230cbe60186717025fb978c5dcdbbc0
albertoSOrtega <alver1998@gmail.com>
2026-03-03 20:23:47 +0100

    feat(MEN): Create navigation to LootBoxOpener menu [MEN-2]

---
+++ Assets/Scripts/MainMenu/BagSlotController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BagSlotController : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private GameObject bagImage;
+    [SerializeField] private GameObject emptySlotText;
+
+    // State
+    private bool isActive = false;
+    private Button button;
+
+    private void Awake()
+    {
+        button = GetComponent<Button>();
+    }
+
+    public void ActivateBagSlot()
+    {
+        button.enabled = true;
+        isActive = true;
+        bagImage.SetActive(true);
+        emptySlotText.SetActive(false);
+    }
+
+    public void DeactivateBagSlot()
+    {
+        button.enabled = false;
+        isActive = false;
+        bagImage.SetActive(false);
+        emptySlotText.SetActive(true);
+    }
+
+}
